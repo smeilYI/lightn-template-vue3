7f956ae55acf577e332ffd55117a7dbb15915e63
A_smile <228641528@qq.com>
2024-02-03 20:04:25 +0800

    update：update file

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { defineComponent } from 'vue';
+import './App.scss';
+// import { devtool } from '@ibiz-template/vue3-components';
+// import { useRouter } from 'vue-router';
+
+export default defineComponent({
+  setup() {
+  },
+  render() {
+    return <router-view/>;
+  },
+});
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,5 @@
 /// <reference types="vite/client" />
+
+declare module 'element-plus/dist/locale/zh-cn.mjs';
+
+declare module 'vue-grid-layout';
